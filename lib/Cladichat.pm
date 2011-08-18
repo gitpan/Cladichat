761a011e488a9eeffb94cd92b36489275d8b2e60
Cladi Di Domenico <kingitalia@gmail.com>
2011-08-18 12:02:46 +0000

    Import of CLADI/Cladichat from CPAN.
    gitpan-cpan-distribution: Cladichat
    gitpan-cpan-version:      
    gitpan-cpan-path:         CLADI/Cladichat.tar.gz
    gitpan-cpan-author:       CLADI
    gitpan-cpan-maturity:     released

---
+++ lib/Cladichat.pm
@@ -0,0 +1,181 @@
+package Cladichat;
+
+use 5.006001;
+use strict;
+use warnings;
+use IO::Socket;
+require Exporter;
+
+our @ISA = qw(Exporter);
+
+# Items to export into callers namespace by default. Note: do not export
+# names by default without a very good reason. Use EXPORT_OK instead.
+# Do not simply export all your public functions/methods/constants.
+
+# This allows declaration	use Cladichat ':all';
+# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
+# will save memory.
+
+our %EXPORT_TAGS = ( 'all' => [ qw(client server) ] );
+
+our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
+
+our @EXPORT = qw(
+	
+);
+
+our $VERSION = '0.01';
+
+sub client
+
+{
+
+    my $macchina_di_arrivo = $_[0];
+    my $porta_di_arrivo    = $_[1];
+
+
+    my $tunnel = IO::Socket::INET->new(
+        Proto    => "tcp",
+        PeerAddr => $macchina_di_arrivo,
+        PeerPort => $porta_di_arrivo
+      )
+      or die
+      "can't connect to port $porta_di_arrivo on $macchina_di_arrivo: $!";
+
+    
+    $tunnel->autoflush(1);
+    my $processo = fork();
+
+    # 1st process
+    if ($processo) {
+
+        # server Talk.....
+        while ( defined( my $paroleserver = <$tunnel> ) ) {
+
+            # standard output...
+            print STDOUT $paroleserver;
+        }
+
+
+        kill( "TERM", $processo );
+
+        die;
+    }
+
+# 2nd process
+    else {
+
+        # client talk......
+        print $tunnel "Hi!\n\r";    #  test words 
+        while ( defined( my $parolemie = <STDIN> ) ) {
+
+            # send
+            chomp $parolemie;
+            print $tunnel "$parolemie\n\r";
+
+        }
+    }
+
+}
+
+sub server
+
+{
+
+    my $mia_porta_in_ascolto = $_[0];
+
+    my $tunnel = IO::Socket::INET->new(
+        Proto     => 'tcp',
+        LocalPort => $mia_porta_in_ascolto,
+        Listen    => SOMAXCONN,
+        Reuse     => 1
+    );
+    die "Non riesco a creare il tunnel" unless $tunnel;
+    while ( my $pc_remoto = $tunnel->accept() ) {
+        $pc_remoto->autoflush(1);
+
+        
+        my $processo = fork();
+
+        #1st process
+        if ($processo) {
+
+            # client speak
+            while ( defined( my $paroleclient = <$pc_remoto> ) ) {
+
+                
+                print STDOUT $paroleclient;
+            }
+            kill( "TERM", $processo );
+        }
+
+        # 2nd process
+        else {
+
+            # server speak
+            while ( defined( my $parolemie = <> ) ) {
+
+                # send
+                print $pc_remoto $parolemie;
+            }
+        }
+    }
+}
+
+
+# Preloaded methods go here.
+
+1;
+__END__
+# Below is stub documentation for your module. You'd better edit it!
+
+=head1 NAME
+
+Cladichat - Perl extension for blah blah blah
+
+=head1 SYNOPSIS
+
+  use Cladichat;
+  blah blah blah
+
+=head1 DESCRIPTION
+
+Stub documentation for Cladichat, created by h2xs. It looks like the
+author of the extension was negligent enough to leave the stub
+unedited.
+
+Blah blah blah.
+
+=head2 EXPORT
+
+None by default.
+
+
+
+=head1 SEE ALSO
+
+Mention other useful documentation such as the documentation of
+related modules or operating system documentation (such as man pages
+in UNIX), or any relevant external documentation such as RFCs or
+standards.
+
+If you have a mailing list set up for your module, mention it here.
+
+If you have a web site set up for your module, mention it here.
+
+Cladi.it
+
+=head1 AUTHOR
+
+Cladi, E<lt>kingitalia@gmail.comE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2011 by Cladi Di Domenico
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.10.1 or,
+at your option, any later version of Perl 5 you may have available.
+
+
+=cut
